cfe8c207265bcffdff750c343fcd1cdc18ca3a82
Resn4R <purview-joust.0x@icloud.com>
2023-10-26 17:23:36 +0100

    Initial Commit

---
+++ Search Func TDD/Search_Func_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_Func_TDDApp.swift
+//  Search Func TDD
+//
+//  Created by Vito Borghi on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Search_Func_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
