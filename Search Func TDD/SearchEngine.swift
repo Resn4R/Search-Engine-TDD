9037c1801a101d2474c8eb41c6dfbe8af0c2c007
Resn4R <purview-joust.0x@icloud.com>
2023-10-27 18:10:33 +0100

    passed all tests + refactoring

--- Search Func TDD/SearchEngine.swift
+++ Search Func TDD/SearchEngine.swift
@@ -13,11 +13,11 @@ struct SearchEngine {
     let cities = ["Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"]
     
     func search(for input: String) -> [String] {
-        guard input.count >= 2 else { return [] }
-        
-        if input.elementsEqual("*") {
+        if input == "*" {
             return cities
         } else {
+            guard input.count >= 2 else { return [] }
+            
             return cities.filter {
                 $0.lowercased().contains(input.lowercased())
             }
